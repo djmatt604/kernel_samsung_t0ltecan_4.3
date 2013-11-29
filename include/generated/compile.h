d057e71728e8bdc476330c84fca02b37d0abb6da
djmatt604 <djmatt604@TI-Cerebro.(none)>
2013-11-28 23:40:24 -0800

    Initial Commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Nov 28 23:31:43 PST 2013"
+#define LINUX_COMPILE_BY "djmatt604"
+#define LINUX_COMPILE_HOST "TI-Cerebro"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
